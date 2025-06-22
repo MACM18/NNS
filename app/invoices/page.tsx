bcfda7a3075810dcf34618cba0a495a8095862e2
Chathura <machathuramadushanka@gmail.com>
2025-06-22 07:57:36 +0530

    Update page.tsx

--- app/invoices/page.tsx
+++ app/invoices/page.tsx
@@ -140,7 +140,7 @@ export default function InvoicesPage() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <Header onAddLineDetails={() => {}} />
+        <Header  />
 
         <main className='flex-1 space-y-6 p-6'>
           {/* Page Header */}
